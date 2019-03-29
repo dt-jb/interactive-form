faf78ade382e6567238d9b068d012cd5c2de69e3
dt_jb <jbrown8013@gmail.com>
2019-03-29 20:10:31 +1100

    Update app.js
    completed project

--- interactive-form-v1/js/app.js
+++ interactive-form-v1/js/app.js
@@ -1,8 +1,13 @@
+
+//Puts focus on name field upon page load
 $('#name').focus();
+
+//Disables 'select method' as a payment method and preselects credit card
+$('#payment option[value="select_method"]').attr('disabled', true);
+$('#payment option[value="credit card"]').attr('selected', true);
 
 //creates a text field when "Job Role" => "Other" is select_method
 $('#other_title').hide();
-
 $('#title').change(function() {
   if ($('#title').val() === "other") {
     $('#other_title').show();
@@ -10,7 +15,6 @@ $('#title').change(function() {
 })
 
 //hides all color choices
-//$('#color option').hide();
 $('#colors-js-puns').hide();
 /*
 unhides colors on change in the tshirt design select menu.  different colors are shown
@@ -21,33 +25,33 @@ $('#design').on('change', function() {
   $('#color option').hide();
   if($("#design").val() === "heart js") {
     $('#color option[value="tomato"]').show();
+    $('#color option[value="tomato"]').attr('selected', true);
     $('#color option[value="steelblue"]').show();
     $('#color option[value="dimgrey"]').show();
   } else if ($("#design").val() === "js puns") {
     //$('#color option').hide();
     $('#color option[value="cornflowerblue"]').show();
+    $('#color option[value="cornflowerblue"]').attr('selected', true);
     $('#color option[value="darkslategrey"]').show();
     $('#color option[value="gold"]').show();
   }
 });
-
+/*
 const $jsFrameworks = $('input[name="js-frameworks"]');
 const $express = $('input[name="express"]');
 const $jsLibs = $('input[name="js-libs"]');
 const $node = $('input[name="node"]');
+*/
 
-
+//
 const $checkbox = $('label input');
 let $dollarTotal = 0;
 const $totalCost = $('<h3>Total: $' + $dollarTotal + '</h3>');
 $('.activities').append($totalCost);
 
-
 $checkbox.on('change', function(event) {
-
   if ($(event.target).is(':checked')) {
     if ($(event.target).parent().text().indexOf('Tuesday') >= 0) {
-
       //checks for conflicting events on Tuesday at 9am-12pm time slot
       if ($(event.target).parent().text().indexOf('9am') >= 0) {
         $checkbox.each(function(index, element){
@@ -57,7 +61,6 @@ $checkbox.on('change', function(event) {
           }
         });
       }
-
       //checks for conflicting events on Tuesday at 1-4pm time slot
       if ($(event.target).parent().text().indexOf('1pm') >= 0) {
         $checkbox.each(function(index, element){
@@ -68,7 +71,6 @@ $checkbox.on('change', function(event) {
         });
       }
     }
-
       //Addes to the dollar total based on the value of the event
       if ($(event.target).parent().text().indexOf('Main') >= 0) {
         $dollarTotal += 200
@@ -90,7 +92,6 @@ $checkbox.on('change', function(event) {
           }
         });
       }
-
       //re-enables events on Tuesday at 1-4pm
       if ($(event.target).parent().text().indexOf('1pm') >= 0) {
         $checkbox.each(function(index, element){
@@ -100,7 +101,6 @@ $checkbox.on('change', function(event) {
         });
       }
     }
-
     //subtracts from the dollar total depending on event upon unchecking
     if ($(event.target).parent().text().indexOf('Main') >= 0) {
       $dollarTotal -= 200
@@ -109,9 +109,7 @@ $checkbox.on('change', function(event) {
     }
     $totalCost.html('<h3>Total: $' + $dollarTotal + '</h3>');
   }
-
 });
-
 
 //shows payment info based on selection, and hides those not selected
 const $selectpayment = $('#payment option[value="select_method"]');
@@ -123,7 +121,6 @@ $paypal.hide();
 $bitcoin.hide();
 
 $('#payment').on('change', function(event) {
-  $selectpayment.attr('disabled', true);
   if ($('#payment option[value="credit card"]').is(':selected')) {
     $creditCard.show();
     $paypal.hide();
@@ -139,9 +136,7 @@ $('#payment').on('change', function(event) {
   }
 });
 
-
 //  VALIDATION SECTION
-
 const nameReg = /^[A-Za-z \-\']+$/i;
 const emailReg = /^[^@.]+@[\w]+\.[a-z]{2,4}$/i;
 const ccReg = /^[0-9]{13,16}$/;
@@ -176,21 +171,20 @@ function preventSub(validator) {
 };
 
 $('button').click(function(event) {
-  if ($('label input:checked').length === 0
-    || $('#name').val()===''
-    || $('#mail').val()==='') {
+  if ($('label input:checked').length === 0) {
     event.preventDefault();
-    alert('Please ensure that you have checked at least one activity box and entered a valid name and email address.');
-  } else {
-    return true;
+    $('.activities').css('background-color', 'red');
+    //alert('Please ensure that you have checked at least one activity box and entered a valid name and email address.');
   }
-});
-
-$('button').click(function(event) {
-  if ($('#payment option[value="select_method"]').is(':selected')) {
+  if ($('#name').val()==='') {
     event.preventDefault();
-    alert('Please ensure that you have selected a payment method.');
-  } else {
+    $('#name').css('border-color', 'red');
+  }
+  if ($('#mail').val()==='') {
+    event.preventDefault();
+    $('#mail').css('border-color', 'red');
+  }
+  else {
     return true;
   }
 });
@@ -331,75 +325,3 @@ $('#cvv').on('input', function () {
       alert('Please select payment method');
   }
 });
-
-//$('#credit-card').on('input', function () {
-  //if ($('#payment option[value="credit card"]').is(':selected')) {
-    //validates cc number
-
-  //}
-//});
-
-
-
-/*
-//Form validation
-let $nameValid = false;
-$('#name').on('input', function () {
-  $nameValid = nameReg.test($('#name').val());
-  validStyle($nameValid, $('#name'));
-  preventSub($nameValid);
-
-  //$('button').click(function(event) {
-  //  if (!$nameValid) {
-  //    event.preventDefault();
-  //  }
-  //});
-
-});
-
-let $emailValid = false;
-$('#mail').on('input', function () {
-  $emailValid = emailReg.test($('#mail').val());
-  validStyle($emailValid, $('#mail'));
-  $('button').click(function(event) {
-    if (!$emailValid) {
-      event.preventDefault();
-    }
-  });
-});
-
-//validates cc number
-let $ccValid = false;
-$('#cc-num').on('input', function () {
-  $ccValid = ccReg.test($('#cc-num').val());
-  validStyle($ccValid, $('#cc-num'));
-  $('button').click(function(event) {
-    if (!$ccValid) {
-      event.preventDefault();
-    }
-  });
-});
-
-//validates zipcode
-let $zipValid = false;
-$('#zip').on('input', function () {
-  $zipValid = zipReg.test($('#zip').val());
-  validStyle($zipValid, $('#zip'));
-  $('button').click(function(event) {
-    if (!$zipValid) {
-      event.preventDefault();
-    }
-  });
-});
-//validates cvv
-let $cvvValid = false;
-$('#cvv').on('input', function () {
-  $cvvValid = cvvReg.test($('#cvv').val());
-  validStyle($cvvValid, $('#cvv'));
-  $('button').click(function(event) {
-    if (!$cvvValid) {
-      event.preventDefault();
-    }
-  });
-});
-*/
